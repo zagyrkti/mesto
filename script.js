6126f77e10a30241aeeb46eb2ff71238ae7f9ccf
Alexandr M <zagyrkti@yandex.ru>
2021-03-30 15:16:13 +0300

    initioal card done

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let profileEditPopupName = document.querySelector(".profile-edit-popup__input_ty
 let profileEditPopupStatus = document.querySelector(".profile-edit-popup__input_type_status");
 let profileEditPopupForm = document.querySelector(".profile-edit-popup__form");
 
-
+// profile popup
 profileEditBtn.addEventListener("click", function () {
   profileEditPopup.classList.add("popup_opened");
   profileEditPopupName.value = profileName.textContent;
@@ -26,3 +26,42 @@ function formSubmitHandler(evt) {
 }
 
 profileEditPopupForm.addEventListener('submit', formSubmitHandler);
+
+// Initial card generation
+
+const initialCards = [
+  {
+    name: "Сладости",
+    link: "./images/sweets_4.jpg"
+  },
+  {
+    name: "Вертушки",
+    link: "./images/windmills.jpg"
+  },
+  {
+    name: "Пряности",
+    link: "images/spice.jpg"
+  },
+  {
+    name: "Плошки",
+    link: "./images/plates.jpg"
+  },
+  {
+    name: "Фонарики",
+    link: "./images/lantern.jpg"
+  },
+  {
+    name: "Базар",
+    link: "./images/marketplace.jpg"
+  },
+];
+
+const cardTemplate = document.querySelector(".card-template").content;
+const cardsContainer = document.querySelector(".cards");
+
+initialCards.forEach((item) => {
+  const cardItem = cardTemplate.querySelector(".cards__item").cloneNode(true);
+  cardItem.querySelector(".cards__photo").src = item.link;
+  cardItem.querySelector(".cards__title").textContent = item.name;
+  cardsContainer.prepend(cardItem);
+})
