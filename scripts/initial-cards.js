cf6f81d16b646989c2895f3c1fa1792fc8bc787b
Alexandr M <zagyrkti@yanderx.ru>
2021-04-03 11:31:44 -0700

    refactoring

---
+++ scripts/initial-cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+        name: "Сладости",
+        link: "./images/sweets_4.jpg"
+    },
+    {
+        name: "Вертушки",
+        link: "./images/windmills.jpg"
+    },
+    {
+        name: "Пряности",
+        link: "images/spice.jpg"
+    },
+    {
+        name: "Плошки",
+        link: "./images/plates.jpg"
+    },
+    {
+        name: "Фонарики",
+        link: "./images/lantern.jpg"
+    },
+    {
+        name: "Базар",
+        link: "./images/marketplace.jpg"
+    },
+];
--- scripts/script.js
+++ scripts/script.js
@@ -11,18 +11,16 @@ const profileEditPopupStatus = document.querySelector(".popup-form__input_data_p
 const profileEditForm = document.querySelector(".popup-form_type_profile-edit");
 
 /*profile popup open*/
-profileEditBtn.addEventListener("click", function () {
+profileEditBtn.addEventListener("click", () => {
   /*open popup*/
-  popupOpen(profileEditPopup);
+  openPopup(profileEditPopup);
   /*get data from profile and paste to inputs*/
   profileEditPopupName.value = profileName.textContent;
   profileEditPopupStatus.value = profileStatus.textContent;
 })
 
 /*profile popup close*/
-profileEditPopupCloseBtn.addEventListener("click", function () {
-  popupClose(profileEditPopup);
-})
+profileEditPopupCloseBtn.addEventListener("click", () => closePopup(profileEditPopup))
 
 /*profile popup action on submit*/
 function profileFormSubmitHandler(evt) {
@@ -31,38 +29,10 @@ function profileFormSubmitHandler(evt) {
   profileName.textContent = profileEditPopupName.value;
   profileStatus.textContent = profileEditPopupStatus.value;
   /*close popup*/
-  popupClose(profileEditPopup);
+  closePopup(profileEditPopup);
 }
 
-profileEditForm.addEventListener('submit', profileFormSubmitHandler);
-
-// Initial cards
-const initialCards = [
-  {
-    name: "Сладости",
-    link: "./images/sweets_4.jpg"
-  },
-  {
-    name: "Вертушки",
-    link: "./images/windmills.jpg"
-  },
-  {
-    name: "Пряности",
-    link: "images/spice.jpg"
-  },
-  {
-    name: "Плошки",
-    link: "./images/plates.jpg"
-  },
-  {
-    name: "Фонарики",
-    link: "./images/lantern.jpg"
-  },
-  {
-    name: "Базар",
-    link: "./images/marketplace.jpg"
-  },
-];
+profileEditForm.addEventListener('submit', profileFormSubmitHandler)
 
 const cardTemplate = document.querySelector(".card-template").content;
 const cardsContainer = document.querySelector(".cards");
@@ -73,7 +43,7 @@ const figCaption = document.querySelector(".popup-figure__caption");
 
 /*generator of initial cards*/
 initialCards.forEach((item) => {
-  createCard(item)
+  prependToCardsContainer(createCard(item));
 })
 
 /* add card block*/
@@ -81,15 +51,11 @@ const addCardBtn = document.querySelector(".profile__add");
 const addCardPopup = document.querySelector(".popup_type_add-card");
 
 /*add card popup open*/
-addCardBtn.addEventListener("click", function () {
-  popupOpen(addCardPopup);
-})
+addCardBtn.addEventListener("click", () => openPopup(addCardPopup))
 
 /*add card popup close*/
 const addCardCloseBtn = document.querySelector(".popup-form__close_owner_add-card");
-addCardCloseBtn.addEventListener("click", function () {
-  popupClose(addCardPopup);
-})
+addCardCloseBtn.addEventListener("click", () => closePopup(addCardPopup))
 
 /*add card form elements*/
 const addCardForm = document.querySelector(".popup-form_type_add-card");
@@ -99,30 +65,33 @@ const addCardLink = document.querySelector(".popup-form__input_data_card-link");
 /*actions on add card form submit*/
 function addCardFormSubmitHandler(evt) {
   evt.preventDefault();
-  createCard({name: addCardName.value, link: addCardLink.value})
-  popupClose(addCardPopup);
+  const card = createCard({name: addCardName.value, link: addCardLink.value});
+  prependToCardsContainer(card);
+  closePopup(addCardPopup);
+  addCardForm.reset();
 }
+
 
 addCardForm.addEventListener('submit', addCardFormSubmitHandler);
 
 /*fade out for popups*/
 /*used to avoid visibility hidden*/
-document.addEventListener('animationend', function (e) {
+document.addEventListener('animationend', (e) => {
   if (e.animationName === 'fade-out') {
     e.target.classList.remove('popup_closed');
   }
 });
 
-function popupOpen(element) {
+function openPopup(element) {
   element.classList.add("popup_opened");
 }
 
-function popupClose(element) {
+function closePopup(element) {
   element.classList.remove("popup_opened");
   element.classList.add("popup_closed");
 }
 
-function createCard (cardData) {
+function createCard(cardData) {
   /*clone template*/
   const cardItem = cardTemplate.querySelector(".cards__item").cloneNode(true);
 
@@ -133,31 +102,30 @@ function createCard (cardData) {
 
   /*card delete*/
   const cardsDeleteBtn = cardItem.querySelector(".cards__delete");
-  cardsDeleteBtn.addEventListener("click", function () {
-    cardItem.remove();
-  })
+  cardsDeleteBtn.addEventListener("click", () => cardItem.remove())
 
   /*card like*/
   const cardLikeBtn = cardItem.querySelector(".cards__like");
-  cardLikeBtn.addEventListener("click", function () {
+  cardLikeBtn.addEventListener("click", () => {
     cardLikeBtn.classList.toggle("cards__like_clicked");
   })
 
   /*fullscreen photo by click on card's img*/
-  cardItem.querySelector(".cards__photo").addEventListener("click", function () {
+  cardItem.querySelector(".cards__photo").addEventListener("click", () => {
     /*open figure popup*/
-    popupOpen(popupFigure);
+    openPopup(popupFigure);
     /*get image data pass to figure*/
     popupFigureImg.src = cardItem.querySelector(".cards__photo").src;
     figCaption.textContent = cardItem.querySelector(".cards__title").textContent;
   })
 
-  /*send card to document*/
+  return cardItem;
+}
+
+function prependToCardsContainer(cardItem) {
   cardsContainer.prepend(cardItem);
 }
 
 /* close photo figure*/
 const figureCloseBtn = document.querySelector(".popup-figure__close");
-figureCloseBtn.addEventListener("click", function () {
-  popupClose(popupFigure);
-})
+figureCloseBtn.addEventListener("click", () => closePopup(popupFigure))
